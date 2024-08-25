335b03d85df60c6086ea3fb9ccb2037aa2dab75b
Denis Savishchev <denis.savishchev@gmail.com>
2024-08-25 11:05:55 +0200

    timers

--- lib/main_provider.dart
+++ lib/main_provider.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainProvider with ChangeNotifier {
 
-  bool day = false;
+  bool day = true;
   bool checkedOne = false;
   bool checkedTwo = false;
   bool checkedThree = false;
+  String startTime = '';
+  String startState = '';
+  String dayDuration = '';
 
-  void switchDay(){
+  String formatDuration(Duration duration) {
+    String twoDigits(int n) {
+      if (n >= 10) return "$n";
+      return "0$n";
+    }
+
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
+
+  void updateTimer() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final duration = prefs.getString('startTime') == null
+        ? DateTime.now().difference(DateTime.now())
+        : DateTime.now().difference(DateTime.parse(prefs.getString('startTime').toString()));
+    dayDuration = formatDuration(duration);
+    notifyListeners();
+  }
+
+  void initDay() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    if(prefs.getString('startTime') == null){
+      day = false;
+      notifyListeners();
+    }else{
+      startTime = DateFormat('HH:mm').format(DateTime.parse(prefs.getString('startTime').toString()));
+      day = prefs.getBool('day')!;
+    }
+  }
+
+  void switchDay() async{
     day = !day;
+    if(day){
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setBool('day', true);
+      await prefs.setString('startTime', DateTime.now().toString());
+      startTime = DateFormat('HH:mm').format(DateTime.parse(prefs.getString('startTime').toString()));
+    }
     notifyListeners();
   }
 
--- lib/widgets/day_switch_widget.dart
+++ lib/widgets/day_switch_widget.dart
@@ -1,13 +1,38 @@
+import 'dart:async';
+
 import 'package:day/main_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../constants.dart';
 
-class DaySwitchWidget extends StatelessWidget {
+class DaySwitchWidget extends StatefulWidget {
   const DaySwitchWidget({
     super.key,
   });
+
+  @override
+  State<DaySwitchWidget> createState() => _DaySwitchWidgetState();
+}
+
+class _DaySwitchWidgetState extends State<DaySwitchWidget> {
+
+  Timer? timer;
+
+  @override
+  void initState() {
+    final data = Provider.of<MainProvider>(context, listen: false);
+    data.initDay();
+    data.updateTimer();
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      data.updateTimer();
+    });
+    super.initState();
+  }
+  @override
+  void dispose() {
+    timer!.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,34 +66,76 @@ class DaySwitchWidget extends StatelessWidget {
                 ),
               ),
               duration: const Duration(milliseconds: 500),
-              child: AnimatedAlign(
-                duration: const Duration(milliseconds: 500),
-                alignment: data.day ? Alignment.centerLeft : Alignment.centerRight,
-                child: GestureDetector(
-                  onTap: () => data.switchDay(),
-                  child: Container(
-                    width: 80,
-                    height: 80,
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(60)),
-                      boxShadow: [
-                        BoxShadow(
-                            color: kBlack.withOpacity(0.1),
-                            spreadRadius: 2,
-                            blurRadius: 1
-                        )
-                      ],
-                    ),
-                    child: AnimatedCrossFade(
-                      firstChild: Image.asset('assets/images/sun.png'),
-                      secondChild: Image.asset('assets/images/moon.png'),
-                      crossFadeState: data.day ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-                      duration: const Duration(milliseconds: 500),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 14,
+                      left: size.width * 0.1,
+                      child: AnimatedOpacity(
+                        opacity: data.day ? 0 : 1,
+                        duration: Duration(milliseconds: data.day ? 200 : 500),
+                        curve: Curves.easeInCubic,
+                        child: const Text('Duration of the last day: 12:37',
+                          style: TextStyle(color: kRed, fontWeight: FontWeight.bold),))),
+                  Positioned(
+                    bottom: 14,
+                    left: size.width * 0.1,
+                    child:
+                    AnimatedOpacity(
+                      opacity: data.day ? 0 : 1,
+                      duration: Duration(milliseconds: data.day ? 200 : 500),
+                        curve: Curves.easeInCubic,
+                      child: const Text('Start your new day',
+                        style: TextStyle(color: kRed, fontWeight: FontWeight.bold),))),
+                  Positioned(
+                      top: 14,
+                      left: size.width * 0.3,
+                      child: AnimatedOpacity(
+                          opacity: data.day ? 1 : 0,
+                          duration: Duration(milliseconds: data.day ? 600 : 200),
+                          curve: Curves.easeInCubic,
+                          child: Text('Your day started at: ${data.startTime}',
+                            style: const TextStyle(color: kBlue, fontWeight: FontWeight.bold),))),
+                  Positioned(
+                      bottom: 14,
+                      left: size.width * 0.3,
+                      child:
+                      AnimatedOpacity(
+                          opacity: data.day ? 1 : 0,
+                          duration: Duration(milliseconds: data.day ? 600 : 200),
+                          curve: Curves.easeInCubic,
+                          child: Text('Day duration: ${data.dayDuration}',
+                            style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),))),
+                  AnimatedAlign(
+                    duration: const Duration(milliseconds: 500),
+                    alignment: data.day ? Alignment.centerLeft : Alignment.centerRight,
+                    child: GestureDetector(
+                      onTap: () => data.switchDay(),
+                      child: Container(
+                        width: 80,
+                        height: 80,
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(Radius.circular(60)),
+                          boxShadow: [
+                            BoxShadow(
+                                color: kBlack.withOpacity(0.1),
+                                spreadRadius: 2,
+                                blurRadius: 1
+                            )
+                          ],
+                        ),
+                        child: AnimatedCrossFade(
+                          firstChild: Image.asset('assets/images/sun.png'),
+                          secondChild: Image.asset('assets/images/moon.png'),
+                          crossFadeState: data.day ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+                          duration: const Duration(milliseconds: 500),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
             ),
           );
